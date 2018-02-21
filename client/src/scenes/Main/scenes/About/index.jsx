dd151070da5ff5d4e22564b128cd5c23d2e44f4b
Thomas Rzipa <t.rzipa@adrom.at>
2018-02-21 12:01:22 +0100

    missed last change

--- client/src/scenes/Main/scenes/About/index.jsx
+++ client/src/scenes/Main/scenes/About/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const About = () => (
-            <Route exact path='/about' >
-            <div className='About'>
+  <div className='About'>
     <h1>About</h1>
   </div>
-  </Route>
 );
 
 export default About;
