1080bc40f5e7ea6efd46acf39db7558f7666d0b4
Thomas Rzipa <t.rzipa@adrom.at>
2018-02-22 12:47:37 +0100

    add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+
 import 'bootstrap/dist/css/bootstrap.css';
 // import fetch from 'whatwg-fetch';
 
 import Main from './scenes/Main';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
 // fetch('/api/food?q=aa')
 //   .then(response => response.json())
@@ -13,9 +16,11 @@ import registerServiceWorker from './registerServiceWorker';
 //   .catch(ex => console.log('parsing failed', ex));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Main />
-  </BrowserRouter>
+  <Provider store={store} >
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'),
 );
 registerServiceWorker();
