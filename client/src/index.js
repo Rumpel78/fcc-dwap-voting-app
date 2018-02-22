f179ffc77f2563d54be073f0cf9f07e080f21573
Thomas Rzipa <t.rzipa@adrom.at>
2018-02-22 12:47:57 +0100

    fix lint

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.css';
 // import fetch from 'whatwg-fetch';
