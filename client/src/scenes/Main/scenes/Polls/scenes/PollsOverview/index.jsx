9f8da91da1d175a43c4a60fba52e82e1b64fc68b
Thomas Rzipa <t.rzipa@adrom.at>
2018-03-08 11:34:39 +0100

    filter only mine polls

--- client/src/scenes/Main/scenes/Polls/scenes/PollsOverview/index.jsx
+++ client/src/scenes/Main/scenes/Polls/scenes/PollsOverview/index.jsx
@@ -8,10 +8,15 @@ import PollApi from '../../services/PollApi';
 class PollsOverview extends React.Component {
   constructor(props) {
     super(props);
+    this.allPolls = [];
 
-    this.state = { polls: [] };
+    this.state = {
+      polls: this.allPolls,
+      onlyMine: false,
+    };
     this.refreshPolls = this.refreshPolls.bind(this);
     this.deletePoll = this.deletePoll.bind(this);
+    this.toggleOnlyMinePolls = this.toggleOnlyMinePolls.bind(this);
   }
 
   componentDidMount() {
@@ -19,9 +24,10 @@ class PollsOverview extends React.Component {
   }
 
   refreshPolls() {
-    this.setState({ polls: [] });
-    PollApi.GetPolls().then(polls =>
-      this.setState({ polls }));
+    PollApi.GetPolls().then((polls) => {
+      this.allPolls = polls;
+      this.showPolls(this.state.onlyMine);
+    });
   }
 
   deletePoll(id) {
@@ -31,12 +37,29 @@ class PollsOverview extends React.Component {
       });
   }
 
+  toggleOnlyMinePolls() {
+    let { onlyMine } = this.state;
+    onlyMine = !onlyMine;
+    this.setState({ onlyMine });
+    this.showPolls(onlyMine);
+  }
+
+  showPolls(onlyMine) {
+    if (onlyMine) {
+      const polls = this.allPolls.filter(p => p.createdBy === this.props.user.name);
+      this.setState({ polls });
+    } else {
+      this.setState({ polls: this.allPolls });
+    }
+  }
+
   render() {
     return (
       <div>
         <ButtonToolbar>
           <Link to='/polls/create'><Button bsStyle='success'>Create new poll</Button></Link>
           <Button onClick={this.refreshPolls} bsStyle='primary'>Refresh</Button>
+          <Button onClick={this.toggleOnlyMinePolls} active={this.state.onlyMine}>Only mine polls</Button>
         </ButtonToolbar>
         <br />
         <br />
