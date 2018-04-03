57916b978d975ab8ebf0d5a7342a5466cc06ac97
Thomas Rzipa <t.rzipa@adrom.at>
2018-04-03 13:04:01 +0200

    Update front page

--- client/src/scenes/Main/scenes/Home/index.jsx
+++ client/src/scenes/Main/scenes/Home/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const Welcome = ({ user }) => (
-  <div>
-    <h1>{ user && `${user.username}, ` }Welcome {user && 'back'} to VoteHub!</h1>
+  <div align="center">
+    <h1>{ user && `${capitalizeFirstLetter(user.username)}, ` }Welcome {user && 'back'} to VoteHub!</h1>
+    <h3>
+      Build, share and vote on polls!
+    </h3>
+    <br />
+    <br />
+    <Link to="/polls"><Button bsSize="large" bsStyle="success">Take me to the polls!</Button></Link>
   </div>
 );
 
