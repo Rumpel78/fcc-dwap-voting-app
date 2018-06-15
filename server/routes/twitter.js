fdf0ca60a71bf3776f018cdd060efe9deec1d9bd
Thomas Rzipa <t.rzipa@adrom.at>
2018-06-15 11:13:37 +0200

    quick fix

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -55,7 +55,7 @@ router.post(
       {
         url: 'https://api.twitter.com/oauth/request_token',
         oauth: {
-          callback: `${req.protocol}://${req.headers.host}/twitter/callback`,
+          callback: `${req.protocol}://${req.headers.host}${config.basePath}/twitter/callback`,
           consumer_key: config.twitter.consumerKey,
           consumer_secret: config.twitter.consumerSecret,
         },
